0a6494f8f33f77956447a6b16f98a6fdb7f10446
Damir Zaripov <zaripov.damir92@mail.ru>
2021-08-25 19:11:47 +0300

    Changed SIGN_IN locator to XPATH

--- locators/authentication.py
+++ locators/authentication.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 
 
 class AuthenticationPageLocators:
-    SIGN_IN = (By.LINK_TEXT, "Вход")
+    SIGN_IN = (By.XPATH, "/html/body/div[2]/nav/ul[2]/li[2]/div/span/a")
     USERNAME = (By.ID, "username")
     PASSWORD = (By.ID, "password")
     LOGIN_SUBMIT = (By.ID, "loginbtn")
